e6a631a59dbff4167b45402f3595555ce33ab99e
volsashgeo <103104366+volsashgeo@users.noreply.github.com>
2022-05-06 22:11:04 +0300

    исправлен синтаксис

--- aroma_2/router.php
+++ aroma_2/router.php
@@ -10,7 +10,7 @@ require_once("php/db.php");
 // }
 
 
-if($url[1] == auth) {
+if($url[1] == "auth") {
     $content = file_get_contents("pages/login.html");
 }else if($url[1] == "reg") {
     $content = file_get_contents("pages/register.html");
@@ -21,6 +21,8 @@ if($url[1] == auth) {
     require_once("pages/users/index.php");
 }else if ($url[1] == "addUser") {
     echo User::addUser($_POST["name"], $_POST["lastname"], $_POST["email"], $_POST["pass"]);
+}else if($url[1] == "authUser") {
+    echo User::authUser($_POST["email"], $_POST["pass"]);
 }else {
     $content = file_get_contents("pages/index.php");  
 }
