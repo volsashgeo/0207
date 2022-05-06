492aed44b6fb37c6594776490bb33ba956210386
volsashgeo <103104366+volsashgeo@users.noreply.github.com>
2022-05-06 12:19:25 +0300

    добавил польз. в Aroma

---
+++ aroma_2/php/classes/User.php
@@ -0,0 +1,56 @@
+<?php 
+class User {
+    private $name;
+    private $lastname;
+    private $email;
+    private $id;
+
+    function __construct($id, $name, $lastname, $email)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->lastname = $lastname;
+        $this->email = $email;
+    }
+    function getId()
+    {
+        return $this->id;
+    }
+    function getName()
+    {
+        return $this->name;
+    }
+    function getLastname()
+    {
+        return $this->lastname;
+    }
+    function getEmail()
+    {
+        return $this->email;
+    }
+
+
+    // Статический метод добавления пользователя
+    static function addUser($name, $lastname, $email, $pass)
+    {
+        global $mysqli;
+        $email = trim(mb_strtolower($email));
+        $pass = trim($pass);
+        $pass = password_hash($pass, PASSWORD_DEFAULT);
+
+        $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email' ");
+        
+        if ($result->num_rows !== 0) {
+            return json_encode(["result"=>"exist"]);
+        }else {
+            return json_encode(["result"=>"success"]);
+        }
+        
+        // echo var_dump($mysqli);
+    }
+
+}
+
+
+
+?>
