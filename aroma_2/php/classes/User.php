940decb58b7353adf459a12432d9be21b3a6e100
volsashgeo <103104366+volsashgeo@users.noreply.github.com>
2022-05-06 19:11:27 +0300

    +статическая функция регистрации пользователя

--- aroma_2/php/classes/User.php
+++ aroma_2/php/classes/User.php
@@ -43,12 +43,30 @@ class User {
         if ($result->num_rows !== 0) {
             return json_encode(["result"=>"exist"]);
         }else {
+            $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
             return json_encode(["result"=>"success"]);
         }
         
         // echo var_dump($mysqli);
     }
-
+    // Cтатический метод авторизации пользователя
+    static function authUser($email, $pass) {
+        global $mysqli;
+        // return "Авторизован"; //Здесь написать функционал авторизации
+        $email = trim(mb_strtolower($email));
+        $pass = trim($pass);
+        $pass = password_hash($pass, PASSWORD_DEFAULT);
+    
+        $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email' /* AND `pass` = '$pass' */ ");
+        // var_dump ($result->num_rows);
+        
+        if($result->num_rows == 0) {
+            return json_encode(["result"=>"not_exist"]);
+    
+        }else {
+            return json_encode(["result"=>"ok"]);
+        }
+    }
 }
 
 
