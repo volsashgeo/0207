dca792113048f68623f72cf3bc4d3ff30b252cf6
volsashgeo <103104366+volsashgeo@users.noreply.github.com>
2022-05-11 14:50:10 +0300

    добавлены переходы на 2 страницы
    добавлены переходы на стр. пользователя и пользователей

--- aroma_2/router.php
+++ aroma_2/router.php
@@ -1,41 +1,35 @@
 <?php
+session_start();
+header('Content-Type: text/html; charset=utf-8');
 
 $url = explode('/', $_SERVER['REQUEST_URI']);
 require_once("php/classes/User.php");
 require_once("php/db.php");
-// if($url[1] == blog) {
-//     require_once("blog.html");
-// }else if($url[1] ==cart) {
-//     require_once("cart.html");
-// }
 
-
-if($url[1] == "auth") {
-    $content = file_get_contents("pages/login.html");
-}else if($url[1] == "reg") {
-    $content = file_get_contents("pages/register.html");
-}else if($url[1] == "blog") {
-    $content = file_get_contents("pages/blog.html");
-
-}else if($url[1] == "users") {
-    require_once("pages/users/index.php");
-}else if ($url[1] == "addUser") {
-    echo User::addUser($_POST["name"], $_POST["lastname"], $_POST["email"], $_POST["pass"]);
-}else if($url[1] == "authUser") {
-    echo User::authUser($_POST["email"], $_POST["pass"]);
-}else {
-    $content = file_get_contents("pages/index.php");  
+if ($url[1] == "auth") {
+  $content = file_get_contents("pages/login.html");
+} else if ($url[1] == "reg") {
+  $content = file_get_contents("pages/register.html");
+} else if ($url[1] == "blog") {
+  $content = file_get_contents("pages/blog.html");
+} else if ($url[1] == "users") {
+  require_once("pages/users/index.php");
+} else if ($url[1] == "addUser") {
+  echo User::addUser($_POST["name"], $_POST["lastname"], $_POST["email"], $_POST["pass"]);
+} else if ($url[1] == "authUser") {
+  echo User::authUser($_POST["email"], $_POST["pass"]);
+} else if ($url[1] == "getUser") {
+  echo User::getUser($_SESSION["id"]);
+} else if ($url[1] == "getUsers") {
+  echo User::getUsers();
+} else {
+  $content = file_get_contents("pages/index.php");
 }
-// $content = file_get_contents("pages/index.php");
-if(!empty($content))
-require_once("template.php");
+
+if (!empty($content))
+  require_once("template.php");
+?>
 
 
 
 
-
-// $url = $url[0];
-// for($i=0; $i<count($url); $i++) {
-
-// echo $url[$i]."<hr>";
-// }
